0fc4db5a16538a0319fec58b87dc035ce78f9ca9
gaetanbo <gaetan.bourlot@laposte.net>
2019-05-23 19:31:23 +0200

    added examples

--- index.js
+++ index.js
@@ -1,9 +1,27 @@
+const http=require('http')
 const express= require('express')
 const app=express()
+
 
 app.get('/',function(req,res) {
 	res.end('Hello World')
 })
+
+//app.post('/',function(req,res){
+//    res.send('post request to homepage')
+//})
+//app.get('/players/',function(req,res) {
+//	res.send('players !')
+//})
+//Route Path : /users/:userId/books/:bookId
+//Request Url : http://localhost:3000/users/34/books/7544
+//req.params: {"userId": "34", "bookId": "7544"}
+//app.get('/users/:userId/books/:bookId',function(req,res,next) {
+//    console.log(('response send in next func')
+//                next()
+//},function(req,res){
+//    res.send(req.params);
+//})
 
 app.listen(5000,function() {
 	console.log('Example app listening on port 5000!')
