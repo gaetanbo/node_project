15dbf1db3d4170c7e145e7a16f1a57362c9d10ff
Guyb <gaetan987@hotmail.fr>
2019-05-24 21:02:24 +0200

    first work on bbiz

--- index.js
+++ index.js
@@ -1,56 +1,49 @@
-const http = require('http');
-const path = require('path');
+//const http = require('http');
+//const path = require('path');
+const bodyParser = require('body-parser');
 const express = require('express');
+const request = require('request');
+const moment = require('moment');
 const app = express();
 
 
-const PORT = process.env.PORT || 3000;
+app.use(express.static('public'));
+app.use(bodyParser.urlencoded({extended: true}));
+app.set('view engine', 'ejs');
 
 app.get('/', function (req, res) {
-    // Answer with homepage
-    //res.end('Hello World')
+    res.render('index');
 });
-
-// Set Static folder
-app.use(express.static('public'));
-
-// Init Middleware
-// app.use(logger);
-//app.get('/player/:id', function (req, res) {
-//    res.send(req.params.id);
-//})
 
 
 app.get('/player', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'player.html'));
+    res.render('player');
 });
 
 app.get('/bbiz', function (req, res) {
-    res.sendFile(path.join(__dirname, 'public', 'bbiz.html'));
+    res.render('bbiz', {info: null, itemName: null, error: null});
 });
 
+app.post('/bbiz', function (req, res) {
+    let itemAsked = req.body.item;
+    //console.log(itemAsked);
 
-app.listen(PORT, () => console.log(`Example app listening on port ${PORT}!`));
+    let url = `https://www.albion-online-data.com/api/v2/stats/prices/${itemAsked}`;
+    request(url, function (err, response, body) {
+        if (err) {
+            res.render('bbiz', {info: null, itemName: null, error: 'Error please try again '});
+        } else {
+            let info = JSON.parse(body);
+            //console.log(info[0]);
+            if (info[0] === undefined) {       // IS NOT AN ARRAY {
+                res.render('bbiz', {info: null, itemName: null, error: 'Error,Item not Found ! '});
+            } else {
+                // On devrait commencer a comparer les prix ici !!
+                res.render('bbiz', {info: info, itemName: itemAsked, error: null});
+            }
+        }
+    });
+});
 
-/*
-
-    EXAMPLES FROM BEST PRATICE expressjs.com
-app.get('/', function (req, res, next) {
-	// do some sync stuff
-	queryDb()
-		.then(function (data) {
-			// handle data
-			return makeCsv(data)
-		})
-		.then(function (csv) {
-			// handle csv
-		})
-		.catch(next)
-})
-
-app.use(function (err, req, res, next) {
-	// handle error
-})
-
-
- */
+const PORT = process.env.PORT || 3000;
+app.listen(PORT, () => console.log(`Test app listening on port ${PORT}!`));
