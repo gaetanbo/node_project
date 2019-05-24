bde420bdb2a3de9f016afaa1a8ed61be9f00676e
Guyb <gaetan987@hotmail.fr>
2019-05-24 17:25:05 +0200

    added bootstrap cdn

--- index.js
+++ index.js
@@ -1,29 +1,39 @@
-const http=require('http')
-const express= require('express')
-const app=express()
+const http = require('http')
+const express = require('express')
+const app = express()
 
+app.use(express.static('public'))
 const PORT = process.env.PORT || 3000;
 
-app.get('/',function(req,res) {	
-	res.end('Hello World')
+app.get('/', function (req, res) {
+    // Answer with homepage
+    res.end('public', index.hmtl);
+    //res.end('Hello World')
 })
 
-//app.post('/',function(req,res){
-//    res.send('post request to homepage')
-//})
-//app.get('/players/',function(req,res) {
-//	res.send('players !')
-//})
-//Route Path : /users/:userId/books/:bookId
-//Request Url : http://localhost:3000/users/34/books/7544
-//req.params: {"userId": "34", "bookId": "7544"}
-//app.get('/users/:userId/books/:bookId',function(req,res,next) {
-//    console.log(('response send in next func')
-//                next()
-//},function(req,res){
-//    res.send(req.params);
-//})
 
-app.listen(PORT,function() {
-	console.log(`Example app listening on port ${PORT}!`);
+app.listen(PORT, function () {
+    console.log(`Example app listening on port ${PORT}!`);
 })
+
+/*
+    EXAMPLES FROM BEST PRATICE expressjs.com
+app.get('/', function (req, res, next) {
+	// do some sync stuff
+	queryDb()
+		.then(function (data) {
+			// handle data
+			return makeCsv(data)
+		})
+		.then(function (csv) {
+			// handle csv
+		})
+		.catch(next)
+})
+
+app.use(function (err, req, res, next) {
+	// handle error
+})
+
+
+ */
