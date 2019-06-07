4ef75ad9fd70bd696023a96bd34d37c448a204be
Maxime Diversay <maxime.diversay@outlook.fr>
2019-06-07 17:30:02 +0200

    [ADD] function to get enchant item ( runes / relic / soul )

--- foundry.js
+++ foundry.js
@@ -1,7 +1,21 @@
 const foundryRoute = require('express').Router();
-const rp = require('request-promise');
+const utils = require("./utils"); 
 
-const traduction = { '@1' : "RUNE", '@2' : "SOUL", "@3" : "RELIC"};
+const enchantPrices = { 
+    '@1' : {
+        name : "RUNE",
+        prices : []
+    }, 
+    '@2' : {
+        name : "SOUL",
+        prices : []
+    },  
+    "@3" : {
+        name : "RELIC",
+        prices : []
+    }, 
+};
+    
 const ratio = [
     {
         types : ["MAIN"],//one-handed weapons
@@ -21,9 +35,58 @@ const ratio = [
     }
 ];
 
-foundryRoute.get('/foundry', (req, res) => {
+function pickWillEnchant(name){
+    let enchant = "@1";
+    if (name.includes("@1")) {
+        enchant = "@2";
+    } else if (name.includes("@2")) {
+        enchant = "@3";
+    } else if (name.includes("@3")) {
+        enchant = "";
+    }
+    return enchant;
+}
 
-    res.render('foundry');
+function pickRatio(name){
+    return ratio.find(x => x.types.some(y => name.includes(y))).value;
+}
+
+function loadEnchantPrices(){
+    return new Promise(async (resolve, reject) => {
+        for(let enchantItem in enchantPrices){
+            for(let i=3; i<9; i++){
+                price = await utils.getPrice(`T${i}_${enchantPrices[enchantItem].name}`, "Caerleon");
+                let obj = JSON.parse(price)[0];
+                if(obj){
+                    enchantPrices[enchantItem].prices[i] = (obj.buy_price_max + obj.buy_price_min)/2;
+                }else{
+                    enchantPrices[enchantItem].prices[i] = -1;
+                }
+            }
+        }
+        resolve();
+    })
+}
+
+foundryRoute.get('/foundry', (req, res) => {
+    if(req.query.category){
+        try{
+            loadEnchantPrices().then( _ => {
+                let usefullItem = utils.getObjectList(req.query.category + ".json");
+                for(let item of usefullItem){
+                    let willEnchant = pickWillEnchant(item.UniqueName);
+                    let ratio = pickRatio(item.UniqueName);
+                    let itemEnchantPrice = enchantPrices[willEnchant].prices[item.UniqueName.substring(1,2)];
+                    let itemPrice = utils.getPrice(item.UniqueName, "Caerleon")
+                };
+                res.render('foundry');
+            })
+        }catch(e){
+            res.render('index');
+        }
+    }else{
+        res.render('index');
+    }
 })
 
 module.exports = foundryRoute;
